f076fb11ad7b55bffc80767adc3cbb67c22d2e3f
linkers697 <music3fhhgyhfgh@gmail.com>
2025-09-13 14:49:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ from pyrogram import filters
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = int(getenv("API_ID"))
+API_ID = getenv("API_ID")
 API_HASH = getenv("API_HASH")
 
 # Get your token from @BotFather on Telegram.
@@ -22,13 +22,13 @@ API_KEY = getenv("API_KEY", 'NxGBNexGenBots5b2d9a')
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
+DURATION_LIMIT_MIN = getenv("DURATION_LIMIT", 60)
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", None))
+LOGGER_ID = getenv("LOGGER_ID", None)
 
 # Get this value from on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 7569863273))
+OWNER_ID = getenv("OWNER_ID", 7569863273)
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
