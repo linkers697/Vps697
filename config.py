d65de0e9ab1075edd1f504e6c4074e6449dacc07
linkers697 <music3fhhgyhfgh@gmail.com>
2025-09-13 14:20:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,8 +42,8 @@ UPSTREAM_REPO = getenv(
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
-    "GIT_TOKEN",
-    "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",
+    "GIT_TOKEN",None
+    
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AuraVisual")
